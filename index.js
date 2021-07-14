ac5d81c7cfaab16a6f311c6c7857ff9ba59f841a
TheAnvils <79940840+TheAnvils@users.noreply.github.com>
2021-07-14 22:48:28 +0300

    MAKE BIG LINE

---
+++ index.js
@@ -0,0 +1,1 @@
+console.sans = async function(string, callback) {const str = ' ' + string;for(let i = 0; i < str.length; i++) {process.stdout.write(str[i]);await new Promise(resolve => setTimeout(resolve, 100));}console.log("");callback();};
