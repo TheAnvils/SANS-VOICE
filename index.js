c8d41ec0a6c48d2868ab363b07953ec6b763884c
TheAnvils <79940840+TheAnvils@users.noreply.github.com>
2021-07-14 22:56:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-console.sans = async function(string, callback) {const str = ' ' + string;for(let i = 0; i < str.length; i++) {process.stdout.write(str[i]);await new Promise(resolve => setTimeout(resolve, 100));}console.log("");callback();};
+const sans = async function(string, callback) {const str = ' ' + string;for(let i = 0; i < str.length; i++) {process.stdout.write(str[i]);await new Promise(resolve => setTimeout(resolve, 100));}console.log("");callback();};
+module.exports = function() {console.sans = sans;};
