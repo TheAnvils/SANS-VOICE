6e25b74df03580d192366049044e7311f72488e2
TheAnvils <79940840+TheAnvils@users.noreply.github.com>
2021-07-14 23:24:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const sans = async function(string, callback) {const str = ' ' + string;for(let i = 0; i < str.length; i++) {process.stdout.write(str[i]);await new Promise(resolve => setTimeout(resolve, 100));}console.log("");if(callback){callback();}else{throw new TypeError('Expected callback, recived undefined')}};
+const sans = async function(string, callback) {if(callback) {const str = ' ' + string;for(let i = 0; i < str.length; i++) {process.stdout.write(str[i]);await new Promise(resolve => setTimeout(resolve, 100));}console.log("");}if(callback){callback();}else{throw new TypeError('Expected callback, recived undefined')}};
 module.exports = function() {console.sans = sans;};
